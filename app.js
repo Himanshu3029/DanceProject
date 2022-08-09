64f710816970d7addb946b1876a81b9d024de2fd
Himanshu <himanshuswami2810@gmail.com>
2022-08-09 10:13:57 +0530

    update

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const bodyparser=require('body-parser')
 // const fs=require('fs')
 const app=express()
 const port = process.env.PORT;
-
+const  db="mongodb+srv://himanshu:gokuss3@@cluster1.bgmoaxi.mongodb.net/DancePROJECT"
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(db);
 
 const ContactSchema = new mongoose.Schema({
     name: String,
